8430c977f399f5a83eb53f1b1916ebac3de92aab
lmcevoy <lmcevoy@stevens.edu>
2020-09-02 09:32:14 -0400

    HelloWorld cpp project in Ubuntu VM (VirtualBox)

---
+++ HelloWorld/helloworld.cpp
@@ -0,0 +1,17 @@
+/*
+ * helloworld.cpp
+ *
+ *  Created on: Sep 2, 2020
+ *      Author: Luke McEvoy
+ */
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "Hello, world!" << endl;
+	return 0;
+}
+
+
+
