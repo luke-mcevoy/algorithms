b8eb3fe2bc355ea7e3064d971fb7e17c73484c14
lmcevoy <lmcevoy@stevens.edu>
2020-09-09 16:45:41 -0400

    testing student class. Error with rounding GPA to 2 decimal points

--- StudentClass/student.cpp
+++ StudentClass/student.cpp
@@ -57,7 +57,55 @@ void print_students(const vector<Student> &students) {
 }
 
 int main() {
-	cout << "Compiled" << endl;
+	string first_name, last_name;
+	float gpa;
+	int id;
+	char repeat;
+	vector<Student> students;
+
+	do {
+		cout << "Enter student's first name: ";
+		cin >> first_name;
+		cout << "Enter student's last name: ";
+		cin >> last_name;
+		gpa = -1;
+		while (gpa < 0 || gpa > 4) {
+			cout << "Enter student's GPA (0.0-4.0): ";
+			cin >> gpa;
+		}
+		cout << "Enter student's ID: ";
+		cin >> id;
+		students.push_back(Student(first_name, last_name, gpa, id));
+		cout << "Add another student to database (Y/N)? ";
+		cin >> repeat;
+	} while (repeat == 'Y' || repeat == 'y');
+
+	cout << endl << "All students: " << endl;
+	print_students(students);
+
+	cout << endl << "Failing students:";
+	vector<Student> failing_students = find_failing_students(students);
+
+	if (failing_students.size() == 0) {
+		cout << " None" << endl;
+	} else {
+		cout << endl;
+		for (auto it = failing_students.cbegin(); it != failing_students.cend(); ++it) {
+			it->print_info();
+		}
+	}
+	// TODO
+	// Print a space and the word 'None' on the same line if no students are failing.
+	// Otherwise, print each failign student on a seperate line.
+
 	return 0;
+
+
+
+
+
+
+
+
 }
 
