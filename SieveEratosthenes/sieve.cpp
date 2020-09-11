83f089660dd8def14b0344546f79a1aafaa483a9
lmcevoy <lmcevoy@stevens.edu>
2020-09-11 12:56:15 -0400

    programming assignment 1 initial commit: base cpp file, bash test script, makefile, assignment pdf

---
+++ SieveEratosthenes/sieve.cpp
@@ -0,0 +1,89 @@
+/*******************************************************************************
+ * Name        : sieve.cpp
+ * Author      :
+ * Date        :
+ * Description : Sieve of Eratosthenes
+ * Pledge      :
+ ******************************************************************************/
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+class PrimesSieve {
+public:
+    PrimesSieve(int limit);
+
+    ~PrimesSieve() {
+        delete [] is_prime_;
+    }
+
+    int num_primes() const {
+        return num_primes_;
+    }
+
+    void display_primes() const;
+
+private:
+    // Instance variables
+    bool * const is_prime_;
+    const int limit_;
+    int num_primes_, max_prime_;
+
+    // Method declarations
+    int count_num_primes() const;
+    void sieve();
+    static int num_digits(int num);
+};
+
+PrimesSieve::PrimesSieve(int limit) :
+        is_prime_{new bool[limit + 1]}, limit_{limit} {
+    sieve();
+}
+
+void PrimesSieve::display_primes() const {
+    // TODO: write code to display the primes in the format specified in the
+    // requirements document.
+}
+
+int PrimesSieve::count_num_primes() const {
+    // TODO: write code to count the number of primes found
+    return 0;
+}
+
+void PrimesSieve::sieve() {
+    // TODO: write sieve algorithm
+}
+
+int PrimesSieve::num_digits(int num) {
+    // TODO: write code to determine how many digits are in an integer
+    // Hint: No strings are needed. Keep dividing by 10.
+    return 0;
+}
+
+int main() {
+    cout << "**************************** " <<  "Sieve of Eratosthenes" <<
+            " ****************************" << endl;
+    cout << "Search for primes up to: ";
+    string limit_str;
+    cin >> limit_str;
+    int limit;
+
+    // Use stringstream for conversion. Don't forget to #include <sstream>
+    istringstream iss(limit_str);
+
+    // Check for error.
+    if ( !(iss >> limit) ) {
+        cerr << "Error: Input is not an integer." << endl;
+        return 1;
+    }
+    if (limit < 2) {
+        cerr << "Error: Input must be an integer >= 2." << endl;
+        return 1;
+    }
+
+    // TODO: write code that uses your class to produce the desired output.
+    return 0;
+}
