5a6f63876589957ba1fe1041b14946a7824b30bc
lmcevoy <lmcevoy@stevens.edu>
2020-09-21 19:18:46 -0400

    initial commit unique letters

---
+++ UniqueLetters/unique.cpp
@@ -0,0 +1,29 @@
+/*******************************************************************************
+ * Name        : unique.cpp
+ * Author      : 
+ * Date        : 
+ * Description : Determining uniqueness of chars with int as bit vector.
+ * Pledge      :
+ ******************************************************************************/
+#include <iostream>
+#include <cctype>
+
+using namespace std;
+
+bool is_all_lowercase(const string &s) {
+    // TODO: returns true if all characters in string are lowercase
+    // letters in the English alphabet; false otherwise.
+}
+
+bool all_unique_letters(const string &s) {
+    // TODO: returns true if all letters in string are unique, that is
+    // no duplicates are found; false otherwise.
+    // You may use only a single int for storage and work with bitwise
+    // and bitshifting operators.
+    // No credit will be given for other solutions.
+}
+
+int main(int argc, char * const argv[]) {
+    // TODO: reads and parses command line arguments.
+    // Calls other functions to produce correct output.
+}
